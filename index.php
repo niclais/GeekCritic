b2ede748e4c38f945c9e027c2a22e3c556dec21b
niclais <56982429+niclais@users.noreply.github.com>
2019-10-24 21:33:58 -0300

    inclusão 2

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>inicial</title>
+
+    <?php
+
+    include('includes/header.php');
+
+
+    ?>
+
+
+</head>
+
+<body>
+
+    <?php
+    include('includes/nav.php');
+    ?>
+
+    <?php
+    include('includes/footer.php');
+    ?>
