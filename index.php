94324878c67b421caa8f8b9d3caa17dacd21e1e3
niclais <56982429+niclais@users.noreply.github.com>
2019-10-24 22:14:01 -0300

    alteração
    alteração card

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>inicial</title>
+    <title>Geek Kriti</title>
 
     <?php
 
@@ -15,6 +15,37 @@
 </head>
 
 <body>
+<div class= "container">
+<div class="card col-md-3" style="">
+  <img class="card-img-top" src=" cap" alt="Imagem de capa do card">
+  <div class="card-body">
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+  </div>
+</div>
+
+<div class="card col-md-3" style="">
+  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Imagem de capa do card">
+  <div class="card-body">
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+  </div>
+</div>
+
+<div class="card col-md-3" style="">
+  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Imagem de capa do card">
+  <div class="card-body">
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+  </div>
+</div>
+
+<div class="card col-md-3" style="">
+  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Imagem de capa do card">
+  <div class="card-body">
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+  </div>
+</div>
+
+
+</div>
 
     <?php
     include('includes/nav.php');
@@ -23,3 +54,5 @@
     <?php
     include('includes/footer.php');
     ?>
+
+</body>
