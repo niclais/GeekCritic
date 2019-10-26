d25d90ddde8fa13193e33895d2c830494ae3e2de
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2019-10-25 21:29:27 -0300

    qaaa
    aa

--- index.php
+++ index.php
@@ -18,37 +18,7 @@
   <?php
     include('includes/nav.php');
     ?>
-<div class= "container">
-<div class="card col-md-3" style="">
-  <img class="card-img-top" src=" cap" alt="Imagem de capa do card">
-  <div class="card-body">
-    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-  </div>
-</div>
 
-<div class="card col-md-3" style="">
-  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Imagem de capa do card">
-  <div class="card-body">
-    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-  </div>
-</div>
-
-<div class="card col-md-3" style="">
-  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Imagem de capa do card">
-  <div class="card-body">
-    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-  </div>
-</div>
-
-<div class="card col-md-3" style="">
-  <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Imagem de capa do card">
-  <div class="card-body">
-    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-  </div>
-</div>
-
-
-</div>
 
     
 
