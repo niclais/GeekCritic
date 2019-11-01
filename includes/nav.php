21e7d50cb3d1fd9d6530f3b5b82397ce29814b7a
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2019-10-31 21:10:43 -0300

    a
    a

--- includes/nav.php
+++ includes/nav.php
@@ -40,8 +40,8 @@
 
     </div>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline- my-2 my-sm-0 color2" type="submit">Search</button>
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+      <button class="btn btn-outline- my-2 my-sm-0 color2" type="submit">Buscar</button>
     </form>
   </nav>
 </header>
