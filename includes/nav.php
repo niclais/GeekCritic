928f6d61041ea46d78829c5f2cebd611212d9e78
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2019-10-31 21:55:20 -0300

    Nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,34 +1,34 @@
 <header>
-  <nav class="navbar-expand-lg navbar-nav navbar-light" style="background-color:white">
-    <a class="navbar-brand" href="#"><img src="#" width="30" height="30" class="d-inline-block align-top" alt=""> Geekritic </a>
+  <nav class="navbar-expand-lg navbar-nav navbar-light nav-menu">
+    <a class="navbar-brand nav-menu-brand" href="#"><img src="#" width="30" height="30" class="d-inline-block align-top" alt=""> Geekritic </a>
     <ul class="navbar-nav mr-auto ">
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">TESTE</a>
+      <li class="nav-item dropdown nav-menu-item">
+        <a class="nav-link dropdown-toggle btn2 btn btn-outline-primary nav-menu-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">GÊNERO</a>
         <div class="dropdown-menu">
           <a class="dropdown-item" href="#">Action</a>
           <a class="dropdown-item" href="#">Another action</a>
           <a class="dropdown-item" href="#">Something else here</a>
-          <div class="dropdown-divider"></div>
+
           <a class="dropdown-item" href="#">Separated link</a>
         </div>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+      <li class="nav-item dropdown nav-menu-item">
+        <a class="nav-link dropdown-toggle btn2 btn btn-outline-primary nav-menu-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">NOTÍCIAS</a>
         <div class="dropdown-menu">
           <a class="dropdown-item" href="#">Action</a>
           <a class="dropdown-item" href="#">Another action</a>
           <a class="dropdown-item" href="#">Something else here</a>
-          <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#">Separated link</a>
+
         </div>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+      <li class="nav-item dropdown nav-menu-item">
+        <a class="nav-link dropdown-toggle btn2 btn btn-outline-primary nav-menu-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"></a>
         <div class="dropdown-menu">
           <a class="dropdown-item" href="#">Action</a>
           <a class="dropdown-item" href="#">Another action</a>
           <a class="dropdown-item" href="#">Something else here</a>
-          <div class="dropdown-divider"></div>
+
           <a class="dropdown-item" href="#">Separated link</a>
         </div>
       </li>
