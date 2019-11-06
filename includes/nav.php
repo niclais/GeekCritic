475a8424968d8526c74d993cee1ccb7493706efe
LuizEduH04 <56982431+LuizEduH04@users.noreply.github.com>
2019-11-06 19:32:22 -0300

    Login botao mudado
    aa

--- includes/nav.php
+++ includes/nav.php
@@ -35,7 +35,7 @@
     </ul>
     <div style="margin-right: 10px">
 
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+      <button type="button" style="background-color:transparent" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
         Login</button>
 
     </div>
