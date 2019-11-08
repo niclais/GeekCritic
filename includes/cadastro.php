036b275fb75251b96d62572039b7ddf31487ffd7
niclais <56982429+niclais@users.noreply.github.com>
2019-11-07 21:50:44 -0300

    a
    a

--- includes/cadastro.php
+++ includes/cadastro.php
@@ -7,28 +7,95 @@
 </head>
 
 <body>
-    <div class="dropdown-menu">
-        <form class="px-4 py-3">
-            <div class="form-group">
-                <label for="exampleDropdownFormEmail1">Endereço de email</label>
-                <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@exemplo.com">
-            </div>
-            <div class="form-group">
-                <label for="exampleDropdownFormPassword1">Senha</label>
-                <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Senha">
-            </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="dropdownCheck">
-                <label class="form-check-label" for="dropdownCheck">
-                    Remember me
-                </label>
-            </div>
-            <button type="submit" class="btn btn-primary">Entrar</button>
-        </form>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">Cadastre-se</a>
-        <a class="dropdown-item" href="#">Esqueceu a senha?</a>
+
+
+<form class="needs-validation" novalidate>
+  <div class= "borda">
+  <div class="form-row">
+    <div class="col-md-4 mb-3">
+      <label for="validationCustom01">Primeiro nome</label>
+      <input type="text" class="form-control" id="validationCustom01" placeholder="Nome" required>
+      <div class="valid-feedback">
+        Tudo certo!
+      </div>
     </div>
+    <div class="col-md-4 mb-3">
+      <label for="validationCustom02">Sobrenome</label>
+      <input type="text" class="form-control" id="validationCustom02" placeholder="Sobrenome" required>
+      <div class="valid-feedback">
+        Tudo certo!
+      </div>
+    </div>
+    <div class="col-md-4 mb-3">
+      <label for="validationCustomUsername">Usuário</label>
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="inputGroupPrepend">@</span>
+        </div>
+        <input type="text" class="form-control" id="validationCustomUsername" placeholder="Usuário" aria-describedby="inputGroupPrepend" required>
+        <div class="invalid-feedback">
+          Por favor, escolha um nome de usuário.
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="col-md-6 mb-3">
+      <label for="validationCustom03">Cidade</label>
+      <input type="text" class="form-control" id="validationCustom03" placeholder="Cidade" required>
+      <div class="invalid-feedback">
+        Por favor, informe uma cidade válida.
+      </div>
+    </div>
+    <div class="col-md-3 mb-3">
+      <label for="validationCustom04">Estado</label>
+      <input type="text" class="form-control" id="validationCustom04" placeholder="Estado" required>
+      <div class="invalid-feedback">
+        Por favor, informe um estado válido.
+      </div>
+    </div>
+    <div class="col-md-3 mb-3">
+      <label for="validationCustom05">CEP</label>
+      <input type="text" class="form-control" id="validationCustom05" placeholder="CEP" required>
+      <div class="invalid-feedback">
+        Por favor, informe um CEP válido.
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+      <label class="form-check-label" for="invalidCheck">
+        Concordo com os termos e condições
+      </label>
+      <div class="invalid-feedback">
+        Você deve concordar, antes de continuar.
+      </div>
+    </div>
+  </div>
+  <button class="btn btn-primary" type="submit">Enviar</button>
+</div>
+
+</form>
+
+<style>
+
+.borda {
+    min-width: 300px;
+    border-width: 2px;
+    border: solid darkgrey;
+    padding: 2px;
+    margin: 2px;
+}
+
+.form-row > .col, .form-row > [class*="col-"]{
+  padding-right: 5px;
+    padding-left: 5px;
+}
+
+
+</style>
+
 </body>
 
 </html>
