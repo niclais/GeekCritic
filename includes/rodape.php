6e7be88a68c137c55a8e1e2670ce59e05dfaba48
julianamartinsenac <56982468+julianamartinsenac@users.noreply.github.com>
2019-11-22 20:41:50 -0300

    Alterações em sala.

--- includes/rodape.php
+++ includes/rodape.php
@@ -2,45 +2,26 @@
     <div class="footer-content">
         <div class="container">
             <div class="row">
-                <div class="col-sm text-center">
-                    <div class="container text-center">SOBRE</div>
-
-
-                    Somos um site de críticas de filmes,<br> abrangendo todos os gêneros <br> e procurando sempre melhorar <br> nosso vasto catálogo
+                <div class="col-sm text-center footer-col">
+                    <b>SOBRE</b><br> 
+                    Somos um site de críticas de filmes,<br> 
+                    abrangendo todos os gêneros <br> 
+                    e procurando sempre melhorar <br> 
+                    nosso vasto catálogo
 
                 </div>
                 <div class="col-sm text-center">
-                    <div class="container text-center">Contato</div>
-                    <li>Email</li>
-                    <div style="margin-left:px"> luizeduh04@gmail.com</div>
-                    <div style=padding-top:10px></div>
-                    <li style="margin-left:px">Telefone</li>
-                    <div style="margin-left:px">999163224</div>
+                    <div class="container text-center"><b>Contato</b></div>
+                    <br>
+                    <img class="icone" src="assets/images/email.png"/> E-mail: luizeduh04@gmail.com<br>
+                    <img class="icone" src="assets/images/telefone.png"/>Telefone: 999163224<br>
                 </div>
 
 
 
             </div>
         </div>
-<hr>
-        <div style="padding-top:50px">
-            <div class="container text-center">
-                <div class="row">
-                    <div class="col-sm ">
-                    <a href="http://localhost/GeekCritic/"  class="badge badge-dark">Home</a>
-                    </div>
-                  
-                    <div class="col-sm" >
-                    <a href="https://mail.google.com/" target="blank" class="badge badge-dark">Contato</a>
-                    </div>
-            
-            </div>
-        </div>
 
+    <div class="footer-bottom">luizeduh04@gmail.com | Designed by Luiz, Lucas, Gustavo e Nicolle</div>
     </div>
-
-
-
-<div class="footer-bottom">luizeduh04@gmail.com | Designed by Luiz, Lucas e Nicole</div>
-</div>
 </div>
