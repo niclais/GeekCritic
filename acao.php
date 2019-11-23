e1cb96fdb2ed04ccf34ac5cce457e013338a9b46
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2019-11-22 21:40:27 -0300

    nav
    nav

--- acao.php
+++ acao.php
@@ -21,148 +21,251 @@
 
 <body>
 
+  <?php
+  include('includes/nav.php');
+  ?>
+  <div class="container confundo">
+    <CENTER>
+      <h1></h1>
+    </CENTER>
+    <div class="row tamanho">
+
+
+      <div class="row">
+        <div class="col-sm-4 ">
+
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://media.comicbook.com/2016/04/madmaxfuryroadrift-177155.png" alt="Imagem de capa do card">
+            <div class="card-body">
+             
+              <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://moviewarden.files.wordpress.com/2018/10/venom-ws-movie-poster-prime.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+              
+              <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-4">
+
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://i.ytimg.com/vi/xlIQYaeY0lM/maxresdefault.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+             
+              <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-4">
+
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://www.jornalspnorte.com.br/enviados/days-gone-review.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+              
+              <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://i.ytimg.com/vi/uoBF-7x69wY/maxresdefault.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+              
+              <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://pplware.sapo.pt/wp-content/uploads/2018/11/BattlefieldV_cover.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+             
+              <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
+  </div>
+  </div>
+
+
+
+
+
+  </div>
+  </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Login</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form class="px-4 py-3">
+            <div class="form-group">
+              <label for="exampleDropdownFormEmail1">Endereço de e-mail</label>
+              <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
+            </div>
+            <div class="form-group">
+              <label for="exampleDropdownFormPassword1">Senha</label>
+              <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+            </div>
+            <div class="form-group">
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="dropdownCheck">
+                <label class="form-check-label" for="dropdownCheck">
+                  Lembrar Senha
+                </label>
+
+              </div>
+            </div>
+            <a class="dropdown-item" style="margin-left:-20px" href="/cadastro.php">Cadastre-se</a>
+          </form>
+          <div>
+
+          </div>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+          <button type="button" class="btn btn-primary">Entrar</button>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+  </div>
+
+
+
+  <style>
+    body {
+
+
+
+
+
+
+
+      background-color: black;
+
+
+
+    }
+
+    .h1,
+    h1 {
+
+      color: #E0EEEE;
+
+      background-color: black;
+
+    }
+
+    .btn2 {
+      width: 150px;
+      height: 40px;
+      font-size: 18px;
+      margin: 5px;
+
+    }
+
+    .btn3 {
+
+      font-size: 35px;
+      background-color: black;
+      color: chartreuse;
+    }
+  </style>
+
+
+
+
+
+
 
 
 
   <?php
-  include('includes/nav.php');
+  include('includes/footer.php');
   ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">Login</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form class="px-4 py-3">
-              <div class="form-group">
-                <label for="exampleDropdownFormEmail1">Endereço de e-mail</label>
-                <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
-              </div>
-              <div class="form-group">
-                <label for="exampleDropdownFormPassword1">Senha</label>
-                <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
-              </div>
-              <div class="form-group">
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="dropdownCheck">
-                  <label class="form-check-label" for="dropdownCheck">
-                    Lembrar Senha
-                  </label>
-
-                </div>
-              </div>
-              <a class="dropdown-item" style="margin-left:-20px"  href="/cadastro.php">Cadastre-se</a>
-            </form>
-            <div>
-
-            </div>
-
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-            <button type="button" class="btn btn-primary">Entrar</button>
-          </div>
-
-        </div>
-
-        </div>
-      </div>
-    </div>
-
-
-
-    <style>
-        body {
-
-
-
-
-
-
-
-            background-color: black;
-           
-
-
-        }
-
-        .h1,
-        h1 {
-
-            color: #E0EEEE;
-
-            background-color: black;
-
-        }
-
-       .btn2{
-           width: 150px;
-           height: 40px;
-           font-size: 18px;
-           margin: 5px;
-
-       }
-       .btn3{
-
-           font-size: 35px;
-           background-color: black;
-           color:chartreuse;
-       }
-
-
-
-     </style>
-
-
-
-
-
-
-
-
-
-    <?php
-    include('includes/footer.php');
-    ?>
 
 </body>
 
