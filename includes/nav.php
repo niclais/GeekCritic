bc7985efdb020c210bb2294e506edadb672c1be0
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2019-11-28 20:22:35 -0300

    a

--- includes/nav.php
+++ includes/nav.php
@@ -1,52 +1,122 @@
-  
-  
-  <header>
+<header>
 
-  <nav class="navbar-expand-lg navbar-nav navbar-light nav-menu">
-    <a class="navbar-brand nav-menu-brand" href="#"><img src="#" width="30" height="30" class="d-inline-block align-top" alt=""> Geekritic </a>
-    <ul class="navbar-nav mr-auto ">
+  <nav class="navbar-expand-lg navbar-nav nav nav-menu b">
+    <a class="navbar-brand nav-menu-brand" href="#"><img src="https:s" width="250" height="100" class="d-inline-block align-top" alt=""></a>
+    <ul class="navbar-nav mr-auto" style="padding-top:35px">
       <li class="nav-item dropdown nav-menu-item">
-        <a style="color: #90a4ae;" class="nav-link dropdown-toggle btn2 btn btn-outline-primary nav-menu-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">GÊNERO</a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="acao.php">Ação</a>
-          <a class="dropdown-item" href="#">Animação</a>
-          <a class="dropdown-item" href="#">Biografia</a>
-          <a class="dropdown-item" href="#">Comédia</a>
-          <a class="dropdown-item" href="#">Ficção Científica</a>
-          <a class="dropdown-item" href="#">Romance</a>
-          <a class="dropdown-item" href="#">Suspense</a>
-          <a class="dropdown-item" href="#">Terror</a>
-
+        <a class="nav-link" style="margin-right:15px" href="index.php" ><b>HOME</b></a>
 
         </div>
       </li>
 
-     
+
       <li class="nav-item dropdown nav-menu-item clearfix">
-      
-        <a style="color: #90a4ae;" class="nav-link dropdown-toggle btn2 btn btn-outline-primary nav-menu-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">NOTÍCIAS</a>
-       
-          
-         
+
+        <a class="nav-link" style="margin-right:15px" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><b>FILMES</b> </a>
+
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">TODOS</a>
+          <a class="dropdown-item" href="#">ACÃO</a>
+          <a class="dropdown-item" href="#">AVENTURA</a>
+          <a class="dropdown-item" href="#">ANIMAÇÃO</a>
+          <a class="dropdown-item" href="#">COMÉDIA</a>
+          <a class="dropdown-item" href="#">DRAMA</a>
+          <a class="dropdown-item" href="#">FICÇÃO</a>
+          <a class="dropdown-item" href="#">ROMANCE</a>
+          <a class="dropdown-item" href="#">SUSPENSE</a>
+          <a class="dropdown-item" href="#">TERROR</a>
+
+
+
         </div>
       </li>
       <li class="nav-item dropdown nav-menu-item">
-        <a style="color: #90a4ae;" class="nav-link dropdown-toggle btn2 btn btn-outline-primary nav-menu-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">EM BREVE</a>
-      
+
+
+        <a class="nav-link " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><b>LANÇAMENTOS</b> </a>
+
+
+
+
+      <li class="nav-item dropdown nav-menu-item clearfix">
+
+        <a class="nav-link  " style="margin-right:15px" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"> <b> <b> </b></a>
+
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href=""></a>
+          <a class="dropdown-item" href=""></a>
+          <a class="dropdown-item" href=""></a>
+          <a class="dropdown-item" href=""></a>
+
+
+
+
+
+
+      <li class="nav-item dropdown nav-menu-item clearfix">
+
+        <a class="nav-link " data-toggle="dropdown" href="troca.php" role="button" aria-haspopup="true" aria-expanded="false"><b></b> </a>
+
+      <li class="nav-item dropdown nav-menu-item">
+
+
+
+
+
+
+
       </li>
     </ul>
-    <div style="margin-right: 10px">
-
-      <button type="button" style="color: #90a4ae; border-color: #90a4ae; background-color:transparent; margin-top:10px" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
-        Login</button>
-
+    <div style="margin-right: 20px">
     </div>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-      <button class="btn btn-outline- my-2 my-sm-0 color2" type="submit">Buscar</button>
-    </form>
-  </nav>
+
+
+
+
+
+ <div style="margin-right: 70px; margin-top:25px"> 
+
+  <button type="button" style="border-color: #90a4ae; background-color:transparent; margin-top:6px" class="btn btn-primary noborda btn-primary1:hover .btn-primary1:not(:disabled):not(.disabled).active, .btn-primary:not(:disabled):not(.disabled):active, .show>.btn-primary.dropdown-toggle" data-toggle="modal" data-target="#exampleModalLong">
+    Login</button>
+
+</div>
+
+</nav>
+</div>
 </header>
 
-  
 
+
+<style>
+  .buscar {
+    margin-top: 3.5px !important;
+    ;
+  }
+
+
+  .noborda {
+    border: none;
+  }
+
+  .a,
+  a {
+    color: black;
+  }
+
+  .nav,
+  nav {
+    background-color: #F8F8FF;
+  }
+
+  .b {
+    padding-top: 5px;
+    border-top: solid 10px #696969;
+    padding-bottom: 5px;
+    border-bottom: solid 10px #696969;
+  }
+
+  .noborda {
+    border: none;
+    color: black;
+  }
+</style>
--- index.php
+++ index.php
@@ -34,174 +34,126 @@
   ?>
 
 
-<div class="text-center nome" style="margin-top:35px">
-  <h1> Últimos Lançamentos </h1>
-</div>
+  <div class="text-center nome" style="margin-top:35px">
+    <h1> LANÇAMENTOS</h1>
+  </div>
 
 
-                <div class="grid row">
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/edf.png" alt="img19"/>
-                        <figcaption>
-                            <h2 class="text-center">O Exterminador <b>do Futuro</b></h2>
-                            <p>Gênero:Ação, Ficção Científica</p>
-                            
-                            <a href="EF.PHP">View more</a>
-                        </figcaption>            
-                    </figure>
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/rl.png" alt="img12"/>
-                        <figcaption>
-                            <h2 class="text-center">O Rei <b>Leão</b></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/br1.png" alt="img12"/>
-                        <figcaption>
-                            <h2 class="text-center">Bohemian <b>Rhapsody</b></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/cr.png" alt="img12"/>
-                        <figcaption>
-                            <h2 class="text-center"><b>Coringa</b></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                </div>
+  <div class="grid row">
+    <figure class="effect-dexter col-md-3">
+      <img src="assets/images/edf.png" alt="img19" />
+      <figcaption>
+        <h2 class="text-center">O Exterminador <b>do Futuro</b></h2>
+        <p>Gênero:Ação, Ficção Científica</p>
+
+        <a href="EF.PHP">View more</a>
+      </figcaption>
+    </figure>
+    <figure class="effect-dexter col-md-3">
+      <img src="assets/images/rl.png" alt="img12" />
+      <figcaption>
+        <h2 class="text-center">O Rei <b>Leão</b></h2>
+        <p>Gênero: animação</p>
+        <a href="#">View more</a>
+      </figcaption>
+    </figure>
+    <figure class="effect-dexter col-md-3">
+      <img src="assets/images/br1.png" alt="img12" />
+      <figcaption>
+        <h2 class="text-center">Bohemian <b>Rhapsody</b></h2>
+        <p>Gênero: Drama</p>
+        <a href="#">View more</a>
+      </figcaption>
+    </figure>
+    <figure class="effect-dexter col-md-3">
+      <img src="assets/images/cr.png" alt="img12" />
+      <figcaption>
+        <h2 class="text-center"><b>Coringa</b></h2>
+        <p>Gênero: suspense</p>
+        <a href="#">View more</a>
+      </figcaption>
+    </figure>
+  </div>
 
 
-                <div class="grid row">
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/vs.png" alt="img19"/>
-                        <figcaption>
-                            <h2 class="text-center">Vingadores <b>Ultimato</b></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/ts.png" alt="img12"/>
-                        <figcaption>
-                            <h2 class="text-center">Toy <b>Story 4</b></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/it.png" alt="img12"/>
-                        <figcaption>
-                            <h2 class="text-center">It: Capítulo <b>Dois</b></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                          <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/cm.png" alt="img12"/>
-                        <figcaption>
-                            <h2 class="text-center">Capitã <b>Marvel</b></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                </div>
-
-                <div class="text-center nome" style="margin-top:35px">
-                <h1> Mais Vistos </h1>
-                </div>
-
-
-                <div class="grid row">
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/edf.png" alt="img19"/>
-                        <figcaption>
-                            <h2>O Exterminador <b>do Futuro</b></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                    <figure class="effect-dexter col-md-3">
-                        <img src="assets/images/it.png" alt="img12"/>
-                        <figcaption>
-                            <h2>Strange <span>Dexter</span></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                    <figure class="effect-dexter col-md-3">
-                        <img src="https://tympanus.net/Development/HoverEffectIdeas/img/12.jpg" alt="img12"/>
-                        <figcaption>
-                            <h2>Strange <span>Dexter</span></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                    <figure class="effect-dexter col-md-3">
-                        <img src="https://tympanus.net/Development/HoverEffectIdeas/img/12.jpg" alt="img12"/>
-                        <figcaption>
-                            <h2>Strange <span>Dexter</span></h2>
-                            <p>Dexter had his own strange way. You could watch him training ants.</p>
-                            <a href="#">View more</a>
-                        </figcaption>            
-                    </figure>
-                </div>
+  <div class="grid row">
+    <figure class="effect-dexter col-md-3">
+      <img src="assets/images/vs.png" alt="img19" />
+      <figcaption>
+        <h2 class="text-center">Vingadores <b>Ultimato</b></h2>
+        <p>Gênero: Ficção Científica</p>
+        <a href="#">View more</a>
+      </figcaption>
+    </figure>
+    <figure class="effect-dexter col-md-3">
+      <img src="assets/images/ts.png" alt="img12" />
+      <figcaption>
+        <h2 class="text-center">Toy <b>Story 4</b></h2>
+        <p>Gênero: animação</p>
+        <a href="#">View more</a>
+      </figcaption>
+    </figure>
+    <figure class="effect-dexter col-md-3">
+      <img src="assets/images/it.png" alt="img12" />
+      <figcaption>
+        <h2 class="text-center">It: Capítulo <b>Dois</b></h2>
+        <p>Gênero: Terror</p>
+        <a href="#">View more</a>
+      </figcaption>
+    </figure>
+    <figure class="effect-dexter col-md-3">
+      <img src="assets/images/cm.png" alt="img12" />
+      <figcaption>
+        <h2 class="text-center">Capitã <b>Marvel</b></h2>
+        <p>Gênero:Ficção Científica</p>
+        <a href="#">View more</a>
+      </figcaption>
+    </figure>
+  </div>
 
 
 
 
 
-<!-- NAO APAGAR MODAL PELO AMOR DE DEUS-->
-    <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">Login</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
+  <center>
+    <div class="else">
+      <div class="mycontainer">
+        <div class="row">
+          <center>
+            <h1 class="hh" style="background: transparent;"> não sabe o que assistir? veja alguns trailers</h1>
+          </center>
+          <iframe width="700"  height="400" src="https://www.youtube.com/embed/4QRdB4RAQMs" style="margin-left:40px" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="col-md-4">
           </div>
-          <div class="modal-body">
-            <form class="px-4 py-3">
-              <div class="form-group">
-                <label for="exampleDropdownFormEmail1">Endereço de e-mail</label>
-                <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
-              </div>
-              <div class="form-group">
-                <label for="exampleDropdownFormPassword1">Senha</label>
-                <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
-              </div>
-              <div class="form-group">
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="dropdownCheck">
-                  <label class="form-check-label" for="dropdownCheck">
-                    Lembrar Senha
-                  </label>
-
-                </div>
-              </div>
-              <a class="dropdown-item" style="margin-left:-20px"  href="cadastro/cadastro.php">Cadastre-se</a>
-            </form>
-            <div>
-
-            </div>
-
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-            <button type="button" class="btn btn-primary">Entrar</button>
-          </div>
-
-        </div>
-
         </div>
       </div>
     </div>
 
+    
+
+
+  </center>
+  <style>
+    body {
+      background-color: black !important;
+
+    }
+
+    .else {
+
+      background-color: #2F4F4F;
+      margin-right:  !important;
+      padding: px;
+      margin-top: 50px;
+      background: #2F4F4F !important;
+      margin-left:
+
+    }
+
+    .hh {
+      margin-left: px;
+    }
+  </style>
 
 
 
@@ -209,18 +161,148 @@
 
 
 
+  <div class="container confundo voce">
+    <CENTER>
+      <h1 class="cor">RECOMENDADOS PARA VOCE</h1>
+    </CENTER>
 
 
+    <div class="row">
+      <div class="col-sm-3 ">
+
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="https://flxt.tmsimg.com/assets/p14568731_v_h8_ac.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text">
+            </p>
+            <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="https://uploads.jovemnerd.com.br/wp-content/uploads/2017/11/reacao-760x428.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text">
+            </p>
+            <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-sm-3">
+
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="https://i.ytimg.com/vi/kghHXz7EGwA/maxresdefault.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text">
+            </p>
+            <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="col-sm-3">
+
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="https://miro.medium.com/max/1068/1*xR_-Dj_n4v0n0jqgY4s2ig.jpeg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title"> </h5>
+            <p class="card-text">
+            </p>
+            <a href="#" class="btn btn-primary">ASSISTIR</a>
+
+          </div>
+        </div>
+      </div>
+    </DIV>
+  </DIV>
+
+
+  <style>
+    .voce {
+
+      padding: 50px;
+
+
+
+    }
+
+    .h1,
+    h1 {
+      color: #90a4ae;
+      background-color: black;
+      font-family: 'Oswald', sans-serif;
+    }
+  </style>
+
+
+
+
+  <!-- NAO APAGAR MODAL PELO AMOR DE DEUS-->
+  <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Login</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form class="px-4 py-3" action="index.php">
+            <div class="form-group">
+              <label for="exampleDropdownFormEmail1">Endereço de e-mail</label>
+              <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
+            </div>
+            <div class="form-group">
+              <label for="exampleDropdownFormPassword1">Senha</label>
+              <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+            </div>
+            <div class="form-group">
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="dropdownCheck">
+                <label class="form-check-label" for="dropdownCheck">
+                  Lembrar Senha
+                </label>
+
+              </div>
+            </div>
+            <a class="dropdown-item" style="margin-left:-20px" href="cadastro.php">Cadastre-se</a>
+          </form>
+          <div>
+
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+          <a href="index.php" type="button" class="btn btn-primary">Entrar</a>
+        </div>
+
+
+        </div>
+  </div>
+    </div>
+
+  </div>
 
 
 
     <?php
-    include('includes/footer.php');
+    include('includes/rodape.php');
     ?>
 
 </body>
-    <?php
-    include('includes/footer.php');
-    ?>
-
+                
 </html>
